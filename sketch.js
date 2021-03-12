74615ad07d79e37f18278e6145d9668a077163a7
IAMZAID22 <73696883+IAMZAID22@users.noreply.github.com>
2021-03-12 17:10:08 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,35 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
+
+var form, player, game;
+
+var c1,c2,c3,c4,cars;
+
+function preload() {
+  playerImg = loadImage("player car.png")
+  enemyImg = loadImage("enemy car.png")
+}
+
+function setup(){
+  canvas = createCanvas(displayWidth - 20,displayHeight - 30);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+
+function draw(){
+  if(playerCount === 4){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+}
